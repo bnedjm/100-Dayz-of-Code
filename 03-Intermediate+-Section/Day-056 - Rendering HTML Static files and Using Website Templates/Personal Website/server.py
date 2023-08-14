cd8bae2d69dcf4f9d79193e157bb6ea27231fad2
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-14 18:21:46 +0200

    day 056 rendered html and css using flask

--- 03-Intermediate+-Section/Day-056 - Rendering HTML Static files and Using Website Templates/Personal Website/server.py
+++ 03-Intermediate+-Section/Day-056 - Rendering HTML Static files and Using Website Templates/Personal Website/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1 style='text-align: center'>Hello, World!</h1>" 
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
