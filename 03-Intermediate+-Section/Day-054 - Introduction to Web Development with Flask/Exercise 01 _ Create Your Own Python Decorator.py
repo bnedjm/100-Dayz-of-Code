3af9ad11347cbc49b35e038a75da8aef22a57a06
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-14 05:18:05 +0200

    day 054 exercise solved

--- 03-Intermediate+-Section/Day-054 - Introduction to Web Development with Flask/Exercise 01 _ Create Your Own Python Decorator.py
+++ 03-Intermediate+-Section/Day-054 - Introduction to Web Development with Flask/Exercise 01 _ Create Your Own Python Decorator.py
@@ -1,14 +1,22 @@
 import time
-current_time = time.time()
-print(current_time)
 
-def speed_calc_decorator():
-    pass
+def speed_calc_decorator(function):
+    def runtime():
+        before_run = time.time()
+        function()
+        after_run = time.time()
+        print(f"{function.__name__} run speed: {after_run - before_run}s")
+    return runtime
 
+@speed_calc_decorator
 def fast_function():
     for i in range(10000000):
         i * i
-        
+
+@speed_calc_decorator  
 def slow_function():
     for i in range(100000000):
         i * i
+
+fast_function()
+slow_function()
