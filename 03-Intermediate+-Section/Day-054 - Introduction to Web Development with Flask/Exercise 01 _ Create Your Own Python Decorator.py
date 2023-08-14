b67102916216cb7bc5e3ef815312a8587f313bcf
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-14 05:03:52 +0200

    day 054 exercise start code

---
+++ 03-Intermediate+-Section/Day-054 - Introduction to Web Development with Flask/Exercise 01 _ Create Your Own Python Decorator.py
@@ -0,0 +1,14 @@
+import time
+current_time = time.time()
+print(current_time)
+
+def speed_calc_decorator():
+    pass
+
+def fast_function():
+    for i in range(10000000):
+        i * i
+        
+def slow_function():
+    for i in range(100000000):
+        i * i
