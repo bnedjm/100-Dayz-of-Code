8060e2f7b13d431931acab88ec438ae8df741813
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-14 06:44:13 +0200

    day 055 exercise solved

--- 03-Intermediate+-Section/Day-055 - HTML & URL Parsing in Flask and the Higher Lower Game/Exercise 01 _ Advanced Decorators.py
+++ 03-Intermediate+-Section/Day-055 - HTML & URL Parsing in Flask and the Higher Lower Game/Exercise 01 _ Advanced Decorators.py
@@ -1,6 +1,15 @@
 # Create the logging_decorator() function 👇
 
-
+def logging_decorator(function):
+    def wrapper(*args, **kwargs):
+        print(F"You called {function.__name__}({args[0]}, {args[1]}, {args[2]})")
+        print(f"It returned: {function(args[0], args[1], args[2])}") 
+    return wrapper
 
 # Use the decorator 👇
 
+@logging_decorator
+def add(a, b, c):
+    return a+b+c
+
+add(1,2,3)
