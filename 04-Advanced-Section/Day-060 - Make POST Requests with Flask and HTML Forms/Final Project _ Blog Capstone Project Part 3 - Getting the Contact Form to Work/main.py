cb42712ac9bd05a0322192436c94522e8b48378d
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-18 00:22:58 +0200

    day 060 final project final code

--- 04-Advanced-Section/Day-060 - Make POST Requests with Flask and HTML Forms/Final Project _ Blog Capstone Project Part 3 - Getting the Contact Form to Work/main.py
+++ 04-Advanced-Section/Day-060 - Make POST Requests with Flask and HTML Forms/Final Project _ Blog Capstone Project Part 3 - Getting the Contact Form to Work/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request
+from smtplib import *
 import requests
+import os
 
-# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+EMAIL_SENDER = os.environ.get("EMAIL_SENDER")
+APP_PASSWORD = os.environ.get("APP_PASSWORD")
+EMAIL_LIST = os.environ.get("EMAIL_LIST")
+
 posts = requests.get("https://api.npoint.io/a90e355e335f23e642d3").json()
 
 app = Flask(__name__)
@@ -16,18 +21,24 @@ def get_all_posts():
 def about():
     return render_template("about.html")
 
-
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "GET":
-        return render_template("contact.html")
+        return render_template("contact.html", method=request.method)
     elif request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        phone = request.form["phone"]
-        message = request.form["message"]
-        print(f"{name}\n{email}\n{phone}\n{message}")
-        return "<h1>Successfully sent your message</h1>"
+        subject = "New Contact request"
+        data = request.form
+        quote = f"Name: {data['name']}\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage: {data['message']}"
+        with SMTP("smtp.gmail.com", port=587) as connect:
+            connect.starttls()
+            connect.login(user=EMAIL_SENDER, password=APP_PASSWORD)
+            connect.sendmail(
+                from_addr=EMAIL_SENDER,
+                to_addrs=EMAIL_LIST,
+                msg=f"Subject: {subject}\n\n"
+                    f"{quote}"
+            )
+        return render_template("contact.html", method=request.method)
 
 @app.route("/post/<int:index>")
 def show_post(index):
