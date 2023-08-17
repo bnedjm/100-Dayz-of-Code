48042600f0989e54629e459d3abc581f3484562f
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-18 01:22:21 +0200

    day 061 WTF-form created successfully

--- 04-Advanced-Section/Day-061 - Building Advanced Forms with Flask-WTForms/Final Project _ Flask Secrets Website/main.py
+++ 04-Advanced-Section/Day-061 - Building Advanced Forms with Flask-WTForms/Final Project _ Flask Secrets Website/main.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
+class MyForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
 
 app = Flask(__name__)
+app.secret_key = "HsereIsASecretKey"
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return render_template('login.html')
+    form = MyForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
