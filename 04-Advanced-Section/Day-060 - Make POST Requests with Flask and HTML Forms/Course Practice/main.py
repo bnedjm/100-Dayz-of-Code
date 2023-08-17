364c647e875f0ce53696a6b9241c63da8ddc2a00
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-17 22:20:54 +0200

    day 060 POST request done

--- 04-Advanced-Section/Day-060 - Make POST Requests with Flask and HTML Forms/Course Practice/main.py
+++ 04-Advanced-Section/Day-060 - Make POST Requests with Flask and HTML Forms/Course Practice/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    return f"<h1>Username : {username}, Password : {password}.</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
