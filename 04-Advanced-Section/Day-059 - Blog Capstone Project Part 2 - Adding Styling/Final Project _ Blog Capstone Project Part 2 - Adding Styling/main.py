0b6bdf1e095afb1d51ad6717f60e4b66c92a2bc3
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-17 20:16:34 +0200

    day 059 step 5 and 6 done

--- 04-Advanced-Section/Day-059 - Blog Capstone Project Part 2 - Adding Styling/Final Project _ Blog Capstone Project Part 2 - Adding Styling/main.py
+++ 04-Advanced-Section/Day-059 - Blog Capstone Project Part 2 - Adding Styling/Final Project _ Blog Capstone Project Part 2 - Adding Styling/main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
+import requests
 
+
+blog_url = "https://api.npoint.io/a90e355e335f23e642d3"
+response = requests.get(url=blog_url)
+response.raise_for_status()
+data = response.json()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=data)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
