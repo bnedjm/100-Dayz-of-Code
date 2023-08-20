9f77f5ba3ab1ec4c2061e04bcfb3fcfaf577ec92
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-20 19:18:56 +0200

    day 063 start code, challenge 1, 2, 3, and 4 done

---
+++ 04-Advanced-Section/Day-063 - Databases and with SQLite and SQLAlchemy/Final Project _ Files Library/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_books=all_books)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == "POST":
+        book =      {
+        "title": request.form['title'],
+        "author": request.form['author'],
+        "rating": int(request.form['rating']),
+        }
+        all_books.append(book)
+        for books in all_books:
+            print(books)
+        redirect(url_for("home"))
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
