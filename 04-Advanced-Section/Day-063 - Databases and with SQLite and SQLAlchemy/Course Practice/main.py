5f5570cb99d82bfbf6f59cc83684162df0802682
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-21 02:29:34 +0200

    day 063 SQLAlchemy CRUD Ops

--- 04-Advanced-Section/Day-063 - Databases and with SQLite and SQLAlchemy/Course Practice/main.py
+++ 04-Advanced-Section/Day-063 - Databases and with SQLite and SQLAlchemy/Course Practice/main.py
@@ -12,20 +12,67 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+db = SQLAlchemy()
 db.init_app(app)
 
 class Books(db.Model):
     __tablename__ = 'books'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), unique=True, nullable=False)
-    author = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
 with app.app_context():
     db.create_all()
     db.session.add(Books(title='Harry Potter', author='J. K. Rowling', rating=9.3))
     db.session.commit()
+
+
+# - - - - - ------------- - - - - - CRUD Ops - - - - - ------------- - - - - - #
+
+# Create A New Record
+
+# with app.app_context():
+#     new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
+
+# Read All Records
+
+# with app.app_context():
+#     result = db.session.execute(db.select(Book).order_by(Book.title))
+#     all_books = result.scalars()
+
+# Read A Particular Record By Query
+
+# with app.app_context():
+#     book = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+
+# Update A Particular Record By Query
+
+# with app.app_context():
+#     book_to_update = db.session.execute(db.select(Book).where(Book.title == "Harry Potter")).scalar()
+#     book_to_update.title = "Harry Potter and the Chamber of Secrets"
+#     db.session.commit() 
+
+# Update A Record By PRIMARY KEY
+
+# book_id = 1
+# with app.app_context():
+#     book_to_update = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+#     # or book_to_update = db.get_or_404(Book, book_id)  
+#     book_to_update.title = "Harry Potter and the Goblet of Fire"
+#     db.session.commit()
+
+# Delete A Particular Record By PRIMARY KEY
+
+# book_id = 1
+# with app.app_context():
+#     book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+#     # or book_to_delete = db.get_or_404(Book, book_id)
+#     db.session.delete(book_to_delete)
+#     db.session.commit()
+
