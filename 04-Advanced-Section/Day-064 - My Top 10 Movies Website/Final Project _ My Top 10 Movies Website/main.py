92631f99c99cc401b1571285bf1c32fa6e2b9f74
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-21 18:46:03 +0200

    day 064 requirement 2 done

--- 04-Advanced-Section/Day-064 - My Top 10 Movies Website/Final Project _ My Top 10 Movies Website/main.py
+++ 04-Advanced-Section/Day-064 - My Top 10 Movies Website/Final Project _ My Top 10 Movies Website/main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, FloatField
+from wtforms.validators import DataRequired, Length
 import requests
 
 '''
@@ -38,6 +38,11 @@ class Movie(db.Model):
     review = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
+class RateMovieForm(FlaskForm):
+    rating = FloatField(label='Your rating out of 10', validators=[DataRequired()])
+    review = StringField(label='Your review', validators=[DataRequired(), Length(max=250)])
+    submit = SubmitField(label='Done')
+
 with app.app_context():
     db.create_all()
     # new_movie = Movie(
@@ -66,10 +71,20 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    with app.app_context():
-        result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
-        all_movies = result.scalars().all()
+    result = db.session.execute(db.select(Movie).order_by(Movie.ranking))
+    all_movies = result.scalars().all()
     return render_template("index.html", movies=all_movies)
+
+@app.route("/edit", methods=['GET', 'POST'])
+def edit():
+    form = RateMovieForm()
+    movie_to_update = db.get_or_404(Movie, request.args.get("index", type=int)) 
+    if form.validate_on_submit():
+            movie_to_update.rating = form.rating.data
+            movie_to_update.review = form.review.data
+            db.session.commit()
+            return redirect(url_for("home"))
+    return render_template("edit.html", form=form, movie=movie_to_update)
 
 
 if __name__ == '__main__':
