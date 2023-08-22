61738a74fb0b2abaa72febf5dc9b5a0465b3aa9c
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-23 01:37:37 +0200

    day 066 start code

---
+++ 04-Advanced-Section/Day-066 - Building Your Own API with RESTful Routing/Final Project _ Cafe API/main.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+'''
+Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+##Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+## HTTP GET - Read Record
+
+## HTTP POST - Create Record
+
+## HTTP PUT/PATCH - Update Record
+
+## HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
