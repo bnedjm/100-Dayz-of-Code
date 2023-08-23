5a62eca927976a168bc804ee7d5920c0458600ed
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-23 22:37:07 +0200

    day 067 DELETE /delete-post/<int:post_id> delete_post()

--- 04-Advanced-Section/Day-067 - Blog Capstone Project Part 3 - RESTful Routing/Final Project _ Blog Capstone Project Part 3 - RESTful Routing/main.py
+++ 04-Advanced-Section/Day-067 - Blog Capstone Project Part 3 - RESTful Routing/Final Project _ Blog Capstone Project Part 3 - RESTful Routing/main.py
@@ -109,8 +109,11 @@ def edit_post(post_id):
 
 # TODO: delete_post() to remove a blog post from the database
 @app.route("/delete-post/<int:post_id>") # type: ignore
-def delete_post():
-    return ""
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id) 
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for("get_all_posts")), 204
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
