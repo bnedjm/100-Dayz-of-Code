d82fbf645ab192543f09257fa9039adcce133f2f
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-23 02:17:13 +0200

    day 066 GET random record - all data

--- 04-Advanced-Section/Day-066 - Building Your Own API with RESTful Routing/Final Project _ Cafe API/main.py
+++ 04-Advanced-Section/Day-066 - Building Your Own API with RESTful Routing/Final Project _ Cafe API/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from random import choice
 
 '''
 Install the required packages first: 
@@ -47,6 +48,25 @@ def home():
     
 
 ## HTTP GET - Read Record
+@app.route("/random", methods=['GET']) # type: ignore
+def random():
+    all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    random_cafe = choice(all_cafes)
+    return jsonify(
+        cafe = {
+            "can_take_calls": random_cafe.can_take_calls,
+            "coffee_price": random_cafe.coffee_price,
+            "has_sockets": random_cafe.has_sockets,
+            "has_toilet": random_cafe.has_toilet,
+            "has_wifi": random_cafe.has_wifi,
+            "id": random_cafe.id,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "map_url": random_cafe.map_url,
+            "name": random_cafe.name,
+            "seats": random_cafe.seats
+            }
+        )
 
 ## HTTP POST - Create Record
 
