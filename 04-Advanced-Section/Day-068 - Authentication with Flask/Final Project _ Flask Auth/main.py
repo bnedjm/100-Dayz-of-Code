c66626e8e00dcf928dd04c283243f42bbc91b3ea
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-24 00:07:24 +0200

    day 068 start code

---
+++ 04-Advanced-Section/Day-068 - Authentication with Flask/Final Project _ Flask Auth/main.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+# CREATE TABLE IN DB
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+ 
+ 
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+def secrets():
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/download')
+def download():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
