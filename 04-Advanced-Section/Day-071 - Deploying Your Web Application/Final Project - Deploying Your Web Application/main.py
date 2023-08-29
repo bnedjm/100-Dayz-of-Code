7c715bbc0b875dad079bf3ba6289a626e897da42
BNedjm <boukhedenna.nedjm@gmail.com>
2023-08-29 18:13:46 +0200

    day 071 use env variables to store sensitive info

--- 04-Advanced-Section/Day-071 - Deploying Your Web Application/Final Project - Deploying Your Web Application/main.py
+++ 04-Advanced-Section/Day-071 - Deploying Your Web Application/Final Project - Deploying Your Web Application/main.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
-
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -28,7 +28,8 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -53,7 +54,8 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
