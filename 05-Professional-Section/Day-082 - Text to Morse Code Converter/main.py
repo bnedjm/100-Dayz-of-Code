a9bc5bb9ba9d3dba9f49ffc7531ad0aad57b0d52
BNedjm <boukhedenna.nedjm@gmail.com>
2023-10-13 18:39:12 +0200

    day 082 morse-text conversion

--- 05-Professional-Section/Day-082 - Text to Morse Code Converter/main.py
+++ 05-Professional-Section/Day-082 - Text to Morse Code Converter/main.py
@@ -1,4 +1,4 @@
-from logo import logo
+from art import tprint
 
 MORSE_CODE_DICT = {"A":".-",
                    "B":"-...",
@@ -47,25 +47,40 @@ MORSE_CODE_DICT = {"A":".-",
 
 def text_morse_converter(message):
     message = message.upper()
-    converted_message =  ""
+    morse_code =  ""
     for char in message:
         if char == " ":
-            converted_message += " "
+            morse_code += " "
             continue
-        converted_message += MORSE_CODE_DICT[char]
-    return converted_message
+        morse_code += MORSE_CODE_DICT[char] + " "
+    return morse_code
 
-def morse_text_converter(code):
-    pass
+def morse_text_converter(morse_code):
+    morse_code = morse_code.split(" ")
+    message =  ""
+    for code in morse_code:
+        if code == "":
+            message += " "
+            continue
+        for key, value in MORSE_CODE_DICT.items():
+            if code == value:
+                message += key
+    return message
 
-user_op = int(input("Which type of conversion do you want to perform?\n1 - text_morse\n2 - morse_text\n3 - Quit\n"))
-print(user_op)
-while(user_op != 3):
-    if user_op == 1:
-        user_message = input("Enter the text that you want to convert.\n")
-        print(text_morse_converter(user_message))
-    elif user_op == 2:
-        user_code = input("Enter the morse code that you want to convert.\n")
-        print(morse_text_converter(user_code))
-    
-    user_op = input("What operation do you wanna perform next?\n1 - text_morse\n2 - morse_text\n3 - Quit\n")
+
+def main():
+    tprint("JUST  MORSE  IT")
+    user_op = int(input("Which type of conversion do you want to perform?\n1 - text_morse\n2 - morse_text\n3 - Quit\n"))
+    while(user_op != 3):
+        if user_op == 1:
+            user_message = input("Enter the text that you want to convert.\n")
+            print(text_morse_converter(user_message))
+        elif user_op == 2:
+            user_code = input("Enter the morse code that you want to convert.\n")
+            print(morse_text_converter(user_code))
+        
+        user_op = int(input("What operation do you wanna perform next?\n1 - text_morse\n2 - morse_text\n3 - Quit\n"))
+
+# if "__name__" == "__main__":
+#     main()
+main()
