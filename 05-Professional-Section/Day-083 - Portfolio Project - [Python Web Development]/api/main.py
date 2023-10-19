2a46468827ad5c39adff05aed6b2a0be7a805621
BNedjm <boukhedenna.nedjm@gmail.com>
2023-10-19 16:05:56 +0200

    day 083 remove profile picture

--- 05-Professional-Section/Day-083 - Portfolio Project - [Python Web Development]/api/main.py
+++ 05-Professional-Section/Day-083 - Portfolio Project - [Python Web Development]/api/main.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
+from smtplib import *
+import requests
+import os
 
+EMAIL_SENDER = os.environ.get("EMAIL_SENDER")
+APP_PASSWORD = os.environ.get("APP_PASSWORD")
+EMAIL_LIST = os.environ.get("EMAIL_LIST")
 
 app = Flask(__name__)
 Bootstrap5(app)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"]) # type: ignore
 def home():
-    return render_template("index.html"), 200
+    if request.method == "GET":
+        return render_template("index.html"), 200
+    elif request.method == "POST":
+        subject = "New Contact request"
+        data = request.form
+        quote = f"Name: {data['name']}\nEmail: {data['email']}\nPhone: {data['phone']}\nMessage: {data['message']}"
+        with SMTP("smtp.gmail.com", port=587) as connect:
+            connect.starttls()
+            connect.login(user=EMAIL_SENDER, password=APP_PASSWORD) # type: ignore
+            connect.sendmail(
+                from_addr=EMAIL_SENDER, # type: ignore
+                to_addrs=EMAIL_LIST, # type: ignore
+                msg=f"Subject: {subject}\n\n"
+                    f"{quote}"
+            )
+        return render_template("index.html"), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
