b4c1948f78622643bbee8ab1243233ea033c4564
BNedjm <boukhedenna.nedjm@gmail.com>
2023-10-21 02:28:30 +0200

    day 083 replace html form with wtforms form

--- 05-Professional-Section/Day-083 - Portfolio Project - [Python Web Development]/api/forms.py
+++ 05-Professional-Section/Day-083 - Portfolio Project - [Python Web Development]/api/forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length
 
 
 class ContactForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(max=100)])
     email = StringField("Email", validators=[DataRequired(), Email(), Length(max=100)])
-    subject = PasswordField("Subject", validators=[DataRequired(), Length(max=100)])
-    message = PasswordField("Message", validators=[DataRequired()])
-    submit = SubmitField("Contact me")
+    subject = StringField("Subject", validators=[DataRequired(), Length(max=100)])
+    message = TextAreaField("Message", validators=[DataRequired()])
