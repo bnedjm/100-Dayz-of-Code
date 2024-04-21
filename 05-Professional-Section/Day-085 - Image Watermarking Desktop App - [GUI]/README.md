4cd2dce80b918cecfcba82b96ce33ee9ac52319a
bnedjm <nedjm.boukhedenna@affirm.com>
2024-04-21 17:33:34 +0100

    day 085 add get_input_images function

---
+++ 05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/README.md
@@ -0,0 +1,1 @@
+# A Desktop program where you can upload images and add a watermark.
--- 05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/main.py
+++ 05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/main.py
@@ -1,1 +1,33 @@
 # start code
+
+# import libraries
+import os
+from PIL import Image
+
+# define all ctss
+PATH_INPUT = "05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/input"
+PATH_OUTPUT = ""
+
+# get input images from input folder
+def get_input_images(path: str):
+    image_files = os.listdir(path)
+    images = []
+    for image_file in image_files:
+        if image_file.endswith((".png", ".jpg", "jpeg")):
+            image_path = os.path.join(PATH_INPUT, image_file)
+            image = Image.open(image_path)
+            images.append(image)
+    return images
+
+# process the images
+images = get_input_images(PATH_INPUT)
+
+for _ in images:
+    _.show()
+
+print(len(images))
+# write the result in the output folder
+
+# other functionalities
+
+# main function
