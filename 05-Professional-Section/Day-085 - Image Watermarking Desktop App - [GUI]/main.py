822ede5dd7a3c511d495b74df74bd6ded3c72e41
bnedjm <nedjm.boukhedenna@affirm.com>
2024-05-22 16:40:57 +0200

    day 085 add main function

--- 05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/main.py
+++ 05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/main.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+import time
 
 PATH_INPUT = "05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/input"
 PATH_OUTPUT = "05-Professional-Section/Day-085 - Image Watermarking Desktop App - [GUI]/output"
@@ -17,14 +18,14 @@ def get_input_images(path: str):
     return images
 
 # watermark an image
-def watermark_image(image: Image, watermark: Image): # type: ignore\
+def watermark_image(image: Image, watermark: Image): # type: ignore
     image_width, image_height = image.size # type: ignore
     wm_width, wm_height = watermark.size # type: ignore
     new_width = image_width*.15
     new_height = wm_height*new_width/wm_width
     size = (int(new_width), int(new_height))
     watermark = watermark.resize(size) # type: ignore
-    image.paste(watermark, (10, 10), watermark.convert("RGBA")) # type: ignore
+    image.paste(watermark, (int(image_width*.02), int(image_height*.02)), watermark.convert("RGBA")) # type: ignore
     return image
 
 # write image in the destination dir
@@ -32,16 +33,18 @@ def save_output_image(image: Image, path: str, image_name: str): # type: ignore
     dest_path = os.path.join(path, f"{image_name}.jpeg")
     image.save(dest_path) # type: ignore
 
-# other functionalities
-
 # main function
-images = get_input_images(PATH_INPUT)
-wm_path = os.path.join(PATH_WM, "wm.png")
-watermark = Image.open(wm_path)
+def main():
+    images = get_input_images(PATH_INPUT)
+    wm_path = os.path.join(PATH_WM, "wm.png")
+    watermark = Image.open(wm_path)
 
-for _ in images:
-    # _.show()
-    _ = watermark_image(_, watermark) # type: ignore
-    save_output_image(_, PATH_OUTPUT, f"wm_output_{images.index(_)}")
+    for image in images:
+        watermarked_image = watermark_image(image, watermark) # type: ignore
+        save_output_image(watermarked_image, PATH_OUTPUT, f"wm_output_{images.index(image)}")
+        print(f"- Image no. {images.index(image)+1} has been watermarked successfuly!")
+        print(images.index(image))
+        time.sleep(1)
 
-print(len(images))
+if __name__ == "__main__":
+    main()
