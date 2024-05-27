326c9e822b90e7dfd4e368d4950ae5fa3a092f0b
bnedjm <nedjm.boukhedenna@affirm.com>
2024-05-27 22:17:44 +0200

    add apikey authentication

--- 05-Professional-Section/Day-088 - Professional Portfolio Project - [Web Development]/app/routes/api.py
+++ 05-Professional-Section/Day-088 - Professional Portfolio Project - [Web Development]/app/routes/api.py
@@ -1,11 +1,27 @@
 from flask import Blueprint, jsonify, request
+from flask_httpauth import HTTPTokenAuth
 from random import choice
 from app import db
 from app.models import Cafe
 
 api_bp = Blueprint("api", __name__)
+auth = HTTPTokenAuth(scheme="ApiKey")
+
+# Example API key store
+api_keys = {
+    "your_api_key_1": "user1",
+    "your_api_key_2": "user2",
+    # Add more keys as needed
+}
+
+@auth.verify_token
+def verify_api_key(api_key):
+    if api_key in api_keys:
+        return api_keys[api_key]
+    return None
 
 @api_bp.route("/random", methods=['GET']) # type: ignore
+@auth.login_required
 def random():
     all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
     random_cafe = choice(all_cafes)
@@ -29,11 +45,13 @@ def random():
     return jsonify(cafe=random_cafe.dictify()), 200
 
 @api_bp.route("/all", methods=['GET']) # type: ignore
+@auth.login_required
 def all():
     all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
     return jsonify(cafes=[cafe.dictify() for cafe in all_cafes]), 200
 
 @api_bp.route("/search", methods=['GET']) # type: ignore
+@auth.login_required
 def search():
     all_cafes = db.session.execute(db.select(Cafe).where(Cafe.location==request.args.get("loc"))).scalars().all()
     if len(all_cafes) == 0:
@@ -44,6 +62,7 @@ def search():
         return jsonify(cafes=[cafe.dictify() for cafe in all_cafes]), 200
 
 @api_bp.route("/add", methods=['POST']) # type: ignore
+@auth.login_required
 def add():
     new_cafe = Cafe(
         name = request.form["name"],
@@ -62,6 +81,7 @@ def add():
     return jsonify(response={"success": "Successfully added the new cafe"}), 201
 
 @api_bp.route("/update-price/<int:cafe_id>", methods=['PATCH']) # type: ignore
+@auth.login_required
 def update_price(cafe_id):
     try:
         cafe_to_update = db.get_or_404(Cafe, cafe_id)
@@ -73,6 +93,7 @@ def update_price(cafe_id):
         return jsonify(response={"success": "Successfully updated the price"}), 200
 
 @api_bp.route("/report-closed/<int:cafe_id>", methods=['DELETE']) # type: ignore
+@auth.login_required
 def report_closed(cafe_id):
     if request.args.get("api-key") == "TopSecretAPIKey":
         try:
